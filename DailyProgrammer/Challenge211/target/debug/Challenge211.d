8208478008631de065f086b21099f909da38997c
Gene Gonzalez <beanmachine44@gmail.com>
2015-04-25 02:25:24 -0400

    Rust Unstable Features & Lost Interest
    Use of unstable Rust features was hindering implementation. Also most
    reddit solutions were done using brute force so it didn't have an
    interesting solution.

--- DailyProgrammer/Challenge211/target/debug/Challenge211.d
+++
@@ -1,4 +0,0 @@
-/home/gene/src/Rust-Practice/DailyProgrammer/Challenge211/target/debug/Challenge211: src/main.rs
-
-/home/gene/src/Rust-Practice/DailyProgrammer/Challenge211/target/debug/Challenge211.d: src/main.rs
-
